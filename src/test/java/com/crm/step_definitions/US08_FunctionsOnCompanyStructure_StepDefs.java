56c95b29cab3fa2b67de32b6da04a870e08a7843
aiturgank <48601244+aiturgank@users.noreply.github.com>
2024-10-30 14:33:10 -0400

    Working on my step defs

--- src/test/java/com/crm/step_definitions/US08_FunctionsOnCompanyStructure_StepDefs.java
+++ src/test/java/com/crm/step_definitions/US08_FunctionsOnCompanyStructure_StepDefs.java
@@ -1,23 +1,25 @@
 package com.crm.step_definitions;
 
 import com.crm.pages.EmployeesPage;
+import com.crm.utilities.BrowserUtils;
+import com.crm.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US08_FunctionsOnCompanyStructure_StepDefs {
     EmployeesPage employeesPage = new EmployeesPage();
 
     @When("the user navigates to the {string} page")
     public void the_user_navigates_to_the_page(String expectedPage) {
-        if(expectedPage.equals("Employees")) {
-            employeesPage.employeesButton.click();
-            //employeesPage.inEmployeesPage(); -> just double checking that the method works
-            Assert.assertTrue(employeesPage.employeesButton.isDisplayed());
-        }else{
-            throw new Error("The page is not displayed");
-        }
+        employeesPage.inEmployeesPage(expectedPage);
     }
     @Then("the {string} header should be visible on the page")
     public void the_header_should_be_visible_on_the_page(String expectedHeader) {
@@ -27,28 +29,24 @@ public class US08_FunctionsOnCompanyStructure_StepDefs {
 // ---------------------------------------------------------------
     @When("user clicks the {string} button")
     public void user_clicks_the_button(String expectedButton) {
-        if(expectedButton.equals("Add department")) {
-            employeesPage.addDepartmentButton.click();
-        }else if(expectedButton.equals("Add")) {
-            employeesPage.addButton.click();
-        }
+        employeesPage.clickButton(expectedButton);
     }
     @When("user enter the department name as {string}")
     public void user_enter_the_department_name_as(String departmentName) {
         employeesPage.departmentName.sendKeys(departmentName);
     }
-    @When("user selects {string} as Parent department")
-    public void user_selects_parent_department(String parentDepartment) {
-        employeesPage.selectParentDepartment(parentDepartment);
+    @When("user selects needed Parent department")
+    public void user_selects_parent_department() {
+        employeesPage.selectParentDepartment();
     }
-
-
-
-//    @Then("the new department {string} should be added successfully")
-//    public void the_new_department_should_be_added_successfully(String string) {
-//        Assert.assertTrue(employeesPage.departmentBox.getText().contains(string));
-//    }
-
-
+    @Then("the new department {string} should be added successfully")
+    public void the_new_department_should_be_added_successfully(String expectedName) {
+        employeesPage.verifyDepartmentTitle(expectedName);
+    }
+//    ---------------------------------------------------------
+    @Then("the {string} button should not be visible to the user")
+    public void the_button_should_not_be_visible_to_the_user(String expectedButton) {
+        employeesPage.addDepartmentNotVisible(expectedButton);
+    }
 
 }
