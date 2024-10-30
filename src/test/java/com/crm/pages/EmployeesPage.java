f0ac459dc903bd9a4375d7bd25a0e59fe2e39c9c
aiturgank <48601244+aiturgank@users.noreply.github.com>
2024-10-30 14:32:56 -0400

    Updated my employees page

--- src/test/java/com/crm/pages/EmployeesPage.java
+++ src/test/java/com/crm/pages/EmployeesPage.java
@@ -1,11 +1,14 @@
 package com.crm.pages;
 
 import com.crm.utilities.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class EmployeesPage {
 
@@ -15,6 +18,15 @@ public class EmployeesPage {
 
     @FindBy(xpath = "//a[@title='Employees']")
     public WebElement employeesButton;
+
+    public void inEmployeesPage(String expectedPage){
+        if(expectedPage.equals("Employees")) {
+            employeesButton.click();
+            Assert.assertTrue(employeesButton.isDisplayed());
+        }else{
+            throw new Error("The page is not displayed");
+        }
+    }
 
     @FindBy(xpath = "//span[@id='pagetitle']")
     public WebElement companyStructureHeader;
@@ -26,26 +38,51 @@ public class EmployeesPage {
     @FindBy(xpath = "//*[@class='popup-window-button popup-window-button-accept']")
     public WebElement addButton;
 
-    @FindBy(xpath = " //input[@id='NAME']")
+    @FindBy(xpath = "//input[@id='NAME']")
     public WebElement departmentName;
 
-    @FindBy(xpath = "//select[@id='IBLOCK_SECTION_ID']")
-    public WebElement parentDepartment;
-
-    public void selectParentDepartment(String parentDepartments) {
-        Select select = new Select(parentDepartment);
-        select.selectByVisibleText(parentDepartments); // Assumes parentDepartment name matches dropdown text
+    public void selectParentDepartment() {
+        WebElement parentDropdown = Driver.getDriver().findElement(By.xpath("//select[@id='IBLOCK_SECTION_ID']"));
+        Select select = new Select(parentDropdown);
+        WebElement selectedOption = select.getFirstSelectedOption();
     }
 
-
-    @FindBy(xpath = "//table[@id='bx_str_level2_table']")
-    public WebElement departmentBox;
-
-
-    public void inEmployeesPage(){
-        employeesButton.click();
+    public void clickButton(String expectedButton){
+        if(expectedButton.equals("Add department")) {
+            addDepartmentButton.click();
+        }else if(expectedButton.equals("Add")) {
+            addButton.click();
+        }
     }
 
+    @FindBy(xpath = "//div[@class='structure-dept-title-text']")
+    public List<WebElement> departmentTitles;
 
+    public void verifyDepartmentTitle(String expectedTitle) {
 
+        boolean found = false;
+        for (WebElement titleElement : departmentTitles) {
+            if (titleElement.getText().trim().equalsIgnoreCase(expectedTitle)) { // Using equalsIgnoreCase for case-insensitivity
+                System.out.println("Department found: " + expectedTitle);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            throw new AssertionError("Department not found: " + expectedTitle);
+        }
+    }
+
+    public void addDepartmentNotVisible(String expectedButton){
+        switch (expectedButton) {
+            case "Add department":
+                boolean isVisible = addDepartmentButton.isDisplayed();
+                if (isVisible) {
+                    throw new AssertionError("The 'Add department' button should not be visible, but it is.");
+                } else {
+                    System.out.println("The 'Add department' button is correctly not visible.");
+                }
+                break;
+        }
+    }
 }
