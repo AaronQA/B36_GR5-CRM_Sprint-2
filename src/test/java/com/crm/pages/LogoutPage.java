d836ef17e3d99aae829aca34959a7cbdcfb8c8a7
shahnoza83 <s_kholova@hotmail.com>
2024-10-31 22:29:13 -0500

    made some changes on LogoutPage file

--- src/test/java/com/crm/pages/LogoutPage.java
+++ src/test/java/com/crm/pages/LogoutPage.java
@@ -1,18 +1,44 @@
 package com.crm.pages;
 
+import com.crm.utilities.BrowserUtils;
+import com.crm.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class LogoutPage extends BasePage{
+import java.util.List;
 
-    @FindBy(xpath= "//div[@id='user-block']")
-    public WebElement profileIcon;
+public class LogoutPage extends HomePage{
+
+    public LogoutPage() {
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+//    @FindBy(xpath= "//div[@id='user-block']")
+//    public WebElement profileIcon;
 
     @FindBy(xpath = "//span[.='Log out']")
     public WebElement logoutButton;
 
     public void logout(){
-        profileIcon.click();
+        userProfile.click();
         logoutButton.click();
     }
+
+    @FindBy(xpath = "//div[@class='menu-popup-items']")
+    public List<WebElement> profileOptions;
+
+    public void profileOptions(){
+        userProfile.click();
+        BrowserUtils.verifyElementDisplayed((By) profileOptions);
+    }
+
+
+
+
+
+
+
 }
