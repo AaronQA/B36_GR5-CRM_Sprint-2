add2ae085de4c62ae31ca84e17ef99689ee2cdee
shahnoza83 <s_kholova@hotmail.com>
2024-10-31 22:29:58 -0500

    updated US02_LogoutPage_StepDefs

--- src/test/java/com/crm/step_definitions/US02_LogoutPage_StepDefs.java
+++ src/test/java/com/crm/step_definitions/US02_LogoutPage_StepDefs.java
@@ -1,19 +1,23 @@
 package com.crm.step_definitions;
 
+import com.crm.pages.HomePage;
 import com.crm.pages.LoginPage;
 import com.crm.pages.LogoutPage;
 import com.crm.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
+
 public class US02_LogoutPage_StepDefs {
 
     //LoginPage loginPage = new LoginPage();
     LogoutPage logoutPage = new LogoutPage();
 
-    @When("user clicks on the profile icon")
-    public void user_clicks_on_the_profile_icon() {
-        logoutPage.profileIcon.click();
+
+    @When("user clicks on the profile dropdown")
+    public void user_clicks_on_the_profile_dropdown() {
+        logoutPage.userProfile.click();
     }
 
     @Then("user clicks the logout button")
@@ -27,8 +31,15 @@ public class US02_LogoutPage_StepDefs {
 
         BrowserUtils.refreshPage();
         BrowserUtils.verifyURLContains("login");
-
     }
+
+
+    @Then("user should be able to see {int} options under profile name")
+    public void user_should_be_able_to_see_options_under_profile_name(Integer numberOfOptions, List<String> expectedOptions) {
+
+        List<String> actualOptions = BrowserUtils.getElementsText(logoutPage.profileOptions);
+    }
+
 
 
 }
