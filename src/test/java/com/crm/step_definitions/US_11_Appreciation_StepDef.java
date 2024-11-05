518c4d3ad71fa5859d9583dbc31a4837604865bd
Nora <nurgul3374@gmail.com>
2024-11-04 23:21:11 -0500

    added code for scenario #2

--- src/test/java/com/crm/step_definitions/US_11_Appreciation_StepDef.java
+++ src/test/java/com/crm/step_definitions/US_11_Appreciation_StepDef.java
@@ -1,14 +1,17 @@
 package com.crm.step_definitions;
 
 import com.crm.pages.AppreciationPage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 public class US_11_Appreciation_StepDef {
 
     AppreciationPage appreciationPage= new AppreciationPage();
 
+    // Scenario #1
     @Given("user clicks on the More dropdown button")
     public void user_clicks_on_the_more_dropdown_button() {
         appreciationPage.more.click();
@@ -23,7 +26,25 @@ public class US_11_Appreciation_StepDef {
     }
     @Then("user sees the Error message {string}")
     public void user_sees_the_error_message(String expectedMessage) {
-        String actualMessage=appreciationPage.emptyErrorNotSpecified.getText();
+        String actualMessage=appreciationPage.errorMessageNoTitle.getText();
         Assert.assertTrue(actualMessage.equals(expectedMessage));
     }
+
+
+
+
+    @And("user removes all recipients")
+    public void userRemovesAllRecipients() {
+        appreciationPage.deleteAllEmployee.click();
+    }
+
+    @Then("user should see the error message {string}")
+    public void user_should_see_the_error_message(String expectedMessage) {
+        String actualMessage1=appreciationPage.errorMessageNoPerson.getText();
+        Assert.assertEquals(actualMessage1,expectedMessage);
+
+    }
+
+
+
 }
