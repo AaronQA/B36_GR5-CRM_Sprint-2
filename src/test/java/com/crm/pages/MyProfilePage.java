5d29b5d78d80c3924aaabd6e30445f3c09e3de36
barno74 <barnosha1974@gmail.com>
2024-11-06 12:19:24 +0500

    MyProfilePage updated

--- src/test/java/com/crm/pages/MyProfilePage.java
+++ src/test/java/com/crm/pages/MyProfilePage.java
@@ -1,37 +1,56 @@
 package com.crm.pages;
 
+import com.crm.utilities.BrowserUtils;
 import com.crm.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class MyProfilePage {
+public class MyProfilePage extends LogoutPage {
+
+
     public MyProfilePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath= "//span[.='My Profile']")
-    public WebElement MyProfile;
+    @FindBy(xpath = "//span[@id='user-name']")
+    public WebElement userProfileIcon;
+
+    @FindBy(xpath = "//span[text()='My Profile']")
+    public WebElement myProfile;
+
+    @FindBy(xpath ="//div[@id='profile-menu-filter']")
+    public  WebElement profileMenuFilter;
 
 
-    @FindBy(xpath = "//a[@class='profile-menu-name']")
-    public WebElement personalUser;
+    @FindBy(xpath = "//span[@id='pagetitle']")
+    public WebElement personalUserEmail;
 
-    @FindBy(xpath = "//a[@class='profile-menu-email']")
-    public WebElement email;
 
-    @FindBy(xpath ="//a[.='General']")
+    @FindBy(xpath = "//a[.='General']")
     public WebElement general;
 
-    @FindBy(xpath ="//a[.='Drive']")
+    @FindBy(xpath = "//a[contains(@class,'profile-menu-item') and contains(text(),'Drive')]")
     public WebElement drive;
 
-    @FindBy(xpath ="//a[.='Tasks']")
+    @FindBy(xpath = "//a[contains(@class,'profile-menu-item') and contains(text(),'Tasks')]")
     public WebElement task;
 
-    @FindBy(xpath ="//a[.='Calendar']")
+    @FindBy(xpath = "//a[contains(@class,'profile-menu-item') and contains(text(),'Calendar')]")
     public WebElement calendar;
 
-    @FindBy(xpath ="//a[.='Conversations']")
-    public WebElement conversation;
+    @FindBy(xpath = "//a[contains(@class,'profile-menu-item') and contains(text(),'Conversations')]")
+    public WebElement conversations;
+
+    public void navigateToProfilePage(){
+        BrowserUtils.waitForPageToLoad(10);
+        userProfileIcon.click();
+        myProfile.click();
+    }
+
+
+    public void click() {
+        myProfile.click();
+
+    }
 }
